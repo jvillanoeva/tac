dee4928eebe115f11c56bebf0a106ec32cc67b36
jvillanoeva <jvillanoeva@gmail.com>
2026-03-22 14:51:30 -0600

    Point API_URL to production api.colectivo.live

--- assets/js/config.js
+++ assets/js/config.js
@@ -9,6 +9,5 @@ const CONFIG = {
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dWJsanJrbXdnZmNhY3NsZGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxOTY5MTQsImV4cCI6MjA4OTc3MjkxNH0._odp4H1WxEVDEnsmcJmKMxQueMt1WJSAv_Tf2BJeHUk',
 
   // Backend API URL
-  API_URL: 'http://localhost:3000',
-  // API_URL: 'https://qvubljrkmwgfcacsldlm.supabase.co',  // production
+  API_URL: 'https://api.colectivo.live',
 };
